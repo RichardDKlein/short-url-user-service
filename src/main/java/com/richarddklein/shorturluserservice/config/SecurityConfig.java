f9fe8d8b9357a70c4f09d84233f65aabca2e76e3
RichardDKlein <RichardDKlein@gmail.com>
2024-05-13 19:32:02 -0500

    Debugging security

--- src/main/java/com/richarddklein/shorturluserservice/config/SecurityConfig.java
+++ src/main/java/com/richarddklein/shorturluserservice/config/SecurityConfig.java
@@ -5,19 +5,34 @@
 
 package com.richarddklein.shorturluserservice.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
 /**
  * The Security @Configuration class.
  *
  * <p>Tells Spring how to construct instances of classes that are needed
- * to implement security.</p>
+ * to implement security (using Spring Security).</p>
  */
+@Order(1)
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
+                .authorizeExchange(authorize -> authorize.anyExchange().permitAll()) // authenticated() // Secure all endpoints
+                .build();
+    }
+
     @Bean
     public BCryptPasswordEncoder
     passwordEncoder() {
