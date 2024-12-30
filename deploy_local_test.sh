60f68eb42b66a39a45a580bac6c468f2d2e4ad44
RichardDKlein <RichardDKlein@gmail.com>
2024-12-30 16:24:28 -0600

    Get local port # from Parameter Store

--- deploy_local_test.sh
+++ deploy_local_test.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-ENV_VARS_FILE="env_test.json"
-PORT=2000
+BASE_URL=$(aws ssm get-parameter \
+    --name /shortUrl/users/baseUrlLocal \
+    --query "Parameter.Value" \
+    --output text)
 
-sam local start-api -p $PORT --env-vars $ENV_VARS_FILE
+PORT_NUMBER=$(echo $BASE_URL | \
+    sed 's/.*host.docker.internal:\([0-9]*\).*/\1/')
+
+echo "Starting ShortUrlUserService on localhost:$PORT_NUMBER"
+
+sam local start-api -p $PORT_NUMBER --env-vars env_test.json
